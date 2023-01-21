d99fca9e8f16cd2bff6ac49418dbf643f3b9e181
Vladislav Tyaglo <vlad.tyaglo95@gmail.com>
2023-01-21 16:52:45 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player'; 
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(setLocalStorageTime, 1000));
+
+player.setCurrentTime(getLocalStorageTime())
+    .catch(error => {
+        console.log(`Error: ${error.name}`)
+    });
+
+function setLocalStorageTime(event) {
+    localStorage.setItem("videoplayer-current-time", `${event.seconds}`);
+}
+
+function getLocalStorageTime() {
+    return localStorage.getItem("videoplayer-current-time");
+}
