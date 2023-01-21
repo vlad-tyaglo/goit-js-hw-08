c6758c5e13061fedd276427217c610084193100f
Vladislav Tyaglo <vlad.tyaglo95@gmail.com>
2023-01-21 14:44:39 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map((item) =>
+        `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a>`).join("");
+
+gallery.innerHTML = galleryMarkup;
+const lightbox = new SimpleLightbox('.gallery a', { close: false, captionDelay: 250, captionsData: 'alt' });
