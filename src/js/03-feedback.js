5c44e14239bd02bc920d159df9cace6b8987727e
Vladislav Tyaglo <vlad.tyaglo95@gmail.com>
2023-01-24 19:54:29 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,62 +1,44 @@
-import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
-const formEl = document.querySelector('.feedback-form');
-const formEmailEl = document.querySelector('[name="email"]');
-const formMessageEl = document.querySelector('[name="message"]');
+const feedbackFormEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+let userData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
-formEmailEl.required = true;
-formMessageEl.required = true;
-
-const LOCALSTORAGE_KEY = "feedback-form-state";
-loadPage();
-const usersData = { email: formEmailEl.value, message: formMessageEl.value };
-
-formEl.addEventListener('input', throttle(onFormInput, 500));
-formEl.addEventListener('submit', onSubmitClick);
-
-function onFormInput(e) {
-    usersData[`${e.target.name}`] = e.target.value;
-    save(LOCALSTORAGE_KEY, usersData);
-}
-
-function loadPage() {
-    const loadDate = load(LOCALSTORAGE_KEY);
-
-    formEmailEl.value = localStorage[LOCALSTORAGE_KEY] ? loadDate.email : '';
-    formMessageEl.value = localStorage[LOCALSTORAGE_KEY] ? loadDate.message : '';
-}
-
-function onSubmitClick(e) {   
-    e.preventDefault();
-    console.log(usersData);
-    formEl.reset();
-    usersData.email = '';
-    usersData.message = '';
-    remove(LOCALSTORAGE_KEY);
-}
-
-function save(key, value) {
-    try {
-      const usersDataSaved = JSON.stringify(value);
-      localStorage.setItem(key, usersDataSaved);
-    } catch (error) {
-      console.error("Saving data error: ", error.message);
+function autofillContactForm() {
+  try {
+    
+    for (const key in userData) {
+      feedbackFormEl.elements[key].value = userData[key];
     }
-};
-
-function load(key) { 
-  try {
-    const usersDataSaved = localStorage.getItem(key);
-    return usersDataSaved === null ? '' : JSON.parse(usersDataSaved);
-  } catch (error) {
-    console.error("Get data error: ", error.message);
+  } catch (err) {
+    console.log(err);
   }
-};
+}
 
-function remove(key) {
-  try {
-    localStorage.removeItem(key);
-  } catch (error) {
-    console.error("Remove data error: ", error.message);
+autofillContactForm(); 
+
+function onFeedbackFormInput(e) {
+  const { name, value } = e.target;
+  userData[name] = value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
+
+function onFeedbackFormSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.target;
+
+  if (email.value === '' || message.value === '') {
+    alert('All fields must be filled');
+  } else {
+    console.log(userData);
+    feedbackFormEl.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    userData = {};
   }
-};
+}
+
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
+feedbackFormEl.addEventListener('input', throttle(onFeedbackFormInput, 500));
